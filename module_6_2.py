eed632d898cbabe2fbc4d62b81c870d7a68d91e3
Igrek141 <igor141@mail.ru>
2024-10-31 01:17:31 +0300

    Add files via upload

--- module_6_2.py
+++ module_6_2.py
@@ -1,5 +1,6 @@
 class Vehicle:
     __COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+
     def __init__(self, owner, __model, __color, __engine_power):
         self.owner = owner
         self.__model = __model
@@ -9,6 +10,7 @@ class Vehicle:
     def get_model(self, __model):
         self.__model = __model
         return
+
     def get_color(self, __color):
         self.__color = __color
 
@@ -28,8 +30,10 @@ class Vehicle:
         else:
             print(f'Нельзя сменить цвет на {new_color}')
 
+
 class Sedan(Vehicle):
     __PASSENGERS_LIMIT = 5
+
 
 vehicle1 = Sedan('Fedos', 'Toyota Mark II', 'blue', 500)
 vehicle1.print_info()
